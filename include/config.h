e15776d3b83356231756a44a480dcfe93a7cb43b
Robbie Linden <robbie.linden@metropolia.fi>
2026-03-13 17:24:20 +0200

    Fixed config.h to have constant header inside of definition

--- include/config.h
+++ include/config.h
@@ -1,9 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "constants.h"
+
 typedef struct {
     int sample_rate;
     int bit_depth;
+    char samples_folder[256];
 } Config;
 
 int load_config(const char *filename, Config *config);
